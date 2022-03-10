ab4bbe90f4601f1fc4d586a2396f56fa05552bfa
PeiyunBai <2483388021@qq.com>
2022-03-10 20:18:36 +0800

    chore: 添加jira-dev-tool

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/*
+ * @Author: baipeiyun
+ * @Date: 2022-01-14 17:59:38
+ * @LastEditors: baipeiyun
+ * @LastEditTime: 2022-03-10 20:08:23
+ * @FilePath: /482mooc-react17/src/App.test.tsx
+ * @Description:
+ */
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+ * @Author: baipeiyun
+ * @Date: 2022-01-14 17:59:38
+ * @LastEditors: baipeiyun
+ * @LastEditTime: 2022-03-10 20:08:37
+ * @FilePath: /482mooc-react17/src/index.tsx
+ * @Description:
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { loadDevTools } from "jira-dev-tool";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+loadDevTools(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
